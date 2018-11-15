960d00ef003339f61e14bdf0224c1f574647ba09
trvrgrhm <trvrgrhm@gmail.com>
2018-11-14 19:00:54 -0700

    started first screen

--- app/src/main/java/com/myapps/toualbiamine/pic2note/MainActivity.java
+++ app/src/main/java/com/myapps/toualbiamine/pic2note/MainActivity.java
@@ -2,14 +2,21 @@ package com.myapps.toualbiamine.pic2note;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //another test comment
         setContentView(R.layout.activity_main);
-        //this is a comment for Testing
-        //this is Seth boiiiii
+
+        title = (TextView)findViewById(R.id.txtTitle);
     }
 }
